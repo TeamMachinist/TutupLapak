2fc96a2fe37d894b4b9cb9fb15d1a323b283520a
fnakhsan <fn.akhsan@gmail.com>
2025-09-13 11:40:32 +0700

    Fix: solve issue from merging dev sqlc

--- internal/queries/auth_queries.sql
+++
@@ -1,8 +0,0 @@
--- name: CheckExistedUserByEmail :one
-SELECT 1 FROM users_auth WHERE email = $1;
-
--- name: GetUserByEmail :one
-SELECT * FROM users_auth WHERE email = $1;
-
--- name: RegisterWithEmail :exec
-INSERT INTO users_auth (id, email, phone, hashed_password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6);
